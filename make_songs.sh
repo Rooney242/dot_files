ca1569e220e88021d13d66ed54da5750bfef83b3
Rooney242 <rooney242psn@gmail.com>
2020-07-14 13:27:53 +0200

    basic structure

--- make_songs.sh
+++ make_songs.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-latexmk -pdf
-latexmk -c
+cd full_songs
+
+echo 'Compiling all songs...'
+latexmk -pdf > /dev/null 2>&1
+
+echo 'Cleaning up extra files...'
+latexmk -c * > /dev/null 2>&1
+
+cd ../tex_files
+
+latexmk -C * > /dev/null 2>&1
